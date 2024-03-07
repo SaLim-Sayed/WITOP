e31d11ae41ae74e6a1e7eedc6e1b8199f87b3593
salimsayed <salemsayed981@gmail.com>
2024-03-08 01:16:39 +0200

    Append filter v2

--- components/Products/Filter.tsx
+++ components/Products/Filter.tsx
@@ -1,9 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { memo, useEffect, useState } from "react";
 import { Checkbox, Button, Input } from "@nextui-org/react";
 import getAllFilter from "@/store/actions/getAllFilter.module";
 import { axiosInstance } from "@/util/axiosConfig";
 import { useProductStore } from "@/store/futures/productStore";
 import { Slider } from "@nextui-org/slider";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from "@nextui-org/react";
 
 interface Filter {
   _id: string;
@@ -11,11 +19,26 @@ interface Filter {
 }
 
 const FilterComponent: React.FC = () => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const { setProducts } = useProductStore();
   const [allFilters, setAllFilters] = useState<Filter[]>([]);
   const [filters, setFilters] = useState<{ [key: string]: boolean }>({});
   const [priceRange, setPriceRange] = useState<[any, any]>([0, 1000]);
 
+  
+  useEffect(() => {
+    getAllFilters();
+  }, []);
+
+  useEffect(() => {
+    // Initialize the filters state when allFilters changes
+    const initialFiltersState = allFilters.reduce((acc, filter, index) => {
+      // Set the first checkbox to checked if it's the first filter
+      const isChecked = index === 0;
+      return { ...acc, [filter.filterName]: isChecked };
+    }, {});
+    setFilters(initialFiltersState);
+  }, [allFilters]);
   const handleCheckboxChange = (key: string) => {
     setFilters((prevFilters) => ({
       ...prevFilters,
@@ -82,77 +105,163 @@ const FilterComponent: React.FC = () => {
     }
   };
 
-  useEffect(() => {
-    getAllFilters();
-  }, []);
+  
 
   return (
     <div className="flex flex-col gap-4">
-      <p className="font-bold text-xl text-cyan-800">Filter Options </p>
-      {allFilters.map((filter) => (
-        <div key={filter._id}>
-          <Checkbox
-            checked={filters[filter.filterName]}
-            onChange={() => handleCheckboxChange(filter.filterName)}
-          >
-            {filter.filterName}
-          </Checkbox>
-        </div>
-      ))}
-      <Slider
-        label="Price Range"
-        step={100}
-        maxValue={1000}
-        minValue={0}
-        size="sm"
-        defaultValue={[0, 1000]}
-        value={priceRange}
-        onChange={handlePriceRangeChange}
-        showTooltip={true}
-        showOutline={true}
-        disableThumbScale={true}
-        formatOptions={{
-          style: "currency",
-          currency: "SAR",
-          maximumFractionDigits: 2,
-        }}
-        tooltipValueFormatOptions={{
-          style: "currency",
-          currency: "SAR",
-          maximumFractionDigits: 0,
-        }}
-        classNames={{
-          base: "max-w-md",
-          filler: "bg-cyan-900",
-          labelWrapper: "mb-2",
-          label: "font-medium text-cyan-700 text-medium",
-          value: "font-medium text-cyan-500 text-small",
-          thumb: [
-            "transition-size",
-            "bg-lightColor-900",
-            "data-[dragging=true]:shadow-lg data-[dragging=true]:shadow-black/20",
-            "data-[dragging=true]:w-7 data-[dragging=true]:h-7 data-[dragging=true]:after:h-6 data-[dragging=true]:after:w-6",
-            "after:bg-cyan-900",
-          ],
-          step: "data-[in-range=true]:bg-black/30 dark:data-[in-range=true]:bg-cyan-900",
-        }}
-        tooltipProps={{
-          offset: 10,
-          placement: "bottom",
-          classNames: {
-            base: [
-              // arrow color
-              "before:bg-cyan-900 after:bg-cyan-900 ",
+      <div className="flex flex-col gap-4 md:hidden">
+        <Button onPress={onOpen} size="lg" className="w-full">
+          Filter{" "}
+        </Button>
+        <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalBody>
+                  <p className="font-bold text-xl mt-6 text-cyan-800">
+                    Filter Options{" "}
+                  </p>
+                  {allFilters.map((filter) => (
+                    <div key={filter._id}>
+                      <Checkbox
+                        checked={filters[filter.filterName]}
+                        onChange={() => handleCheckboxChange(filter.filterName)}
+                      >
+                        {filter.filterName}
+                      </Checkbox>
+                    </div>
+                  ))}
+                  <Slider
+                    label="Price Range"
+                    step={50}
+                    maxValue={1000}
+                    minValue={0}
+                    size="sm"
+                    defaultValue={[0, 1000]}
+                    value={priceRange}
+                    onChange={handlePriceRangeChange}
+                    showTooltip={true}
+                    showOutline={true}
+                    disableThumbScale={true}
+                    formatOptions={{
+                      style: "currency",
+                      currency: "SAR",
+                      maximumFractionDigits: 2,
+                    }}
+                    tooltipValueFormatOptions={{
+                      style: "currency",
+                      currency: "SAR",
+                      maximumFractionDigits: 0,
+                    }}
+                    classNames={{
+                      base: "max-w-md",
+                      filler: "bg-cyan-900",
+                      labelWrapper: "mb-2",
+                      label: "font-medium text-cyan-700 text-medium",
+                      value: "font-medium text-cyan-500 text-small",
+                      thumb: [
+                        "transition-size",
+                        "bg-lightColor-900",
+                        "data-[dragging=true]:shadow-lg data-[dragging=true]:shadow-black/20",
+                        "data-[dragging=true]:w-7 data-[dragging=true]:h-7 data-[dragging=true]:after:h-6 data-[dragging=true]:after:w-6",
+                        "after:bg-cyan-900",
+                      ],
+                      step: "data-[in-range=true]:bg-black/30 dark:data-[in-range=true]:bg-cyan-900",
+                    }}
+                    tooltipProps={{
+                      offset: 10,
+                      placement: "bottom",
+                      classNames: {
+                        base: [
+                          // arrow color
+                          "before:bg-cyan-900 after:bg-cyan-900 ",
+                        ],
+                        content: ["py-2 shadow-xl", "text-white bg-cyan-900"],
+                      },
+                    }}
+                  />
+                  <Button
+                    className="bg-cyan-400 text-white text-xl"
+                    onClick={applyFilters}
+                  >
+                    Apply Filters
+                  </Button>
+                </ModalBody>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
+      </div>
+      <div className="  flex-col gap-4 hidden md:flex">
+        <p className="font-bold text-xl text-cyan-800">Filter Options </p>
+        {allFilters.map((filter) => (
+          <div key={filter._id}>
+            <Checkbox
+              checked={filters[filter.filterName]}
+              onChange={() => handleCheckboxChange(filter.filterName)}
+            >
+              {filter.filterName}
+            </Checkbox>
+          </div>
+        ))}
+        <Slider
+          label="Price Range"
+          step={50}
+          maxValue={1000}
+          minValue={0}
+          size="sm"
+          defaultValue={[0, 1000]}
+          value={priceRange}
+          onChange={handlePriceRangeChange}
+          showTooltip={true}
+          showOutline={true}
+          disableThumbScale={true}
+          formatOptions={{
+            style: "currency",
+            currency: "SAR",
+            maximumFractionDigits: 2,
+          }}
+          tooltipValueFormatOptions={{
+            style: "currency",
+            currency: "SAR",
+            maximumFractionDigits: 0,
+          }}
+          classNames={{
+            base: "max-w-md",
+            filler: "bg-cyan-900",
+            labelWrapper: "mb-2",
+            label: "font-medium text-cyan-700 text-medium",
+            value: "font-medium text-cyan-500 text-small",
+            thumb: [
+              "transition-size",
+              "bg-lightColor-900",
+              "data-[dragging=true]:shadow-lg data-[dragging=true]:shadow-black/20",
+              "data-[dragging=true]:w-7 data-[dragging=true]:h-7 data-[dragging=true]:after:h-6 data-[dragging=true]:after:w-6",
+              "after:bg-cyan-900",
             ],
-            content: ["py-2 shadow-xl", "text-white bg-cyan-900"],
-          },
-        }}
-      />
-      <Button className="bg-cyan-400 text-white text-xl" onClick={applyFilters}>
-        Apply Filters
-      </Button>
+            step: "data-[in-range=true]:bg-black/30 dark:data-[in-range=true]:bg-cyan-900",
+          }}
+          tooltipProps={{
+            offset: 10,
+            placement: "bottom",
+            classNames: {
+              base: [
+                // arrow color
+                "before:bg-cyan-900 after:bg-cyan-900 ",
+              ],
+              content: ["py-2 shadow-xl", "text-white bg-cyan-900"],
+            },
+          }}
+        />
+        <Button
+          className="bg-cyan-400 text-white text-xl"
+          onClick={applyFilters}
+        >
+          Apply Filters
+        </Button>
+      </div>
     </div>
   );
 };
 
-export default FilterComponent;
+export default memo(FilterComponent);
--- components/Products/Products.tsx
+++ components/Products/Products.tsx
@@ -10,7 +10,7 @@ import getProducts from "@/store/actions/products.module";
 import { useParams } from "next/navigation";
 import { useLocale } from "next-intl";
 import GCardSkeleton from "../Global/Loaders/GCardSkeleton";
-import Layer from "../Global/Ui/Layer"; 
+import Layer from "../Global/Ui/Layer";
 import FilterComponent from "./Filter";
 import { useProductStore } from "@/store/futures/productStore";
 
@@ -18,56 +18,59 @@ export default function Product() {
   const { category } = useParams();
   const locale = useLocale();
   const dir = locale == "ar" ? "rtl" : "ltr";
-  const {products, setProducts} = useProductStore();
+  const { products, setProducts } = useProductStore();
   const [total, setTotal] = useState(1);
 
   const geData = async () => {
     const server = await getProducts({ category });
     setProducts(server?.products);
     setTotal(server?.totalPage);
-     
   };
   useEffect(() => {
     geData();
   }, []);
   return (
     <div>
-       <Headings type={products && products[0]?.category} />
+      <Headings type={products && products[0]?.category} />
 
-<Center>
-  <div className="flex flex-col  gap-8">
-    <Layer
-      title={
-        <div className=" text-3xl font-bold">
-          {products && products[0]?.category}
-        </div>
-      }
-    />
+      <Center>
+        <div className="flex flex-col  gap-8">
+          <Layer
+            title={
+              <div className=" text-3xl font-bold">
+                {products && products[0]?.category}
+              </div>
+            }
+          />
 
-           <div className="flex">
-            <div className="w-[20%]">
-                <FilterComponent/>
+          <div className="flex flex-col md:flex-row gap-4">
+            <div className="w-[80%]  mx-auto  md:w-[20%]  ">
+              <FilterComponent />
             </div>
-           
-          <div dir={dir} className="flex flex-1   flex-wrap  justify-around  gap-4">
-            {products.length > 0
-              ? products.map((product) => (
-                  <GCard
-                    key={product?._id}
-                    id={product?._id}
-                    price={product?.price}
-                    title={product?.productName}
-                    desc={product?.description}
-                    img={product?.images[0]}
-                    category={product?.category}
-                  />
-                ))
-              : Array.from({ length: 4 }).map((_index: any) => (
-                  <div key={_index}>
-                    <GCardSkeleton />
-                  </div>
-                ))}
-          </div></div>
+
+            <div
+              dir={dir}
+              className="flex flex-1   flex-wrap  justify-around  gap-4"
+            >
+              {products.length > 0
+                ? products.map((product) => (
+                    <GCard
+                      key={product?._id}
+                      id={product?._id}
+                      price={product?.price}
+                      title={product?.productName}
+                      desc={product?.description}
+                      img={product?.images[0]}
+                      category={product?.category}
+                    />
+                  ))
+                : Array.from({ length: 4 }).map((_index: any) => (
+                    <div key={_index}>
+                      <GCardSkeleton />
+                    </div>
+                  ))}
+            </div>
+          </div>
           <div className="flex justify-center">
             <Pagination showControls total={total || 10} initialPage={1} />
           </div>
@@ -76,4 +79,4 @@ export default function Product() {
     </div>
   );
 }
-3
+3;
