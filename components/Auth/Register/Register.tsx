961f275422f62a25552a4ac1a0b78954e1734e9c
Salim Sayed <salemsayed981@gmail.com>
2024-09-07 11:52:35 +0300

    fix error

--- components/Auth/Register/Register.tsx
+++ components/Auth/Register/Register.tsx
@@ -1,18 +1,22 @@
 "use client";
-import { Card } from "@nextui-org/react";
+import { Card, Spinner } from "@nextui-org/react";
 import RegisterForm from "./RegisterForm";
 import Center from "@/components/Global/Ui/Center";
 import Title from "@/components/Global/Ui/Title";
 import { useTranslations } from "next-intl";
+import { Suspense } from "react";
 
 export default function Register() {
   const tr = useTranslations("Auth");
+  
   return (
     <Center>
       <Title exSt="mt-4" title={tr("Register/Title")} />
       <div className="flex justify-center mb-4 ">
         <Card className="flex w-full md:w-[50%]    p-4 items-center flex-col  gap-[40px]">
-          <RegisterForm />
+          <Suspense fallback={<Spinner />}>
+            <RegisterForm />
+          </Suspense>
         </Card>
       </div>
     </Center>
